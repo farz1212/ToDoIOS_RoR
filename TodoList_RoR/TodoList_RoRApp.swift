70975bc047af6b34ef2721c3397a1f4effebc54a
farz1212 <farzaad1212@gmail.com>
2024-08-22 14:10:50 -0400

    Initial Commit

---
+++ TodoList_RoR/TodoList_RoRApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodoList_RoRApp.swift
+//  TodoList_RoR
+//
+//  Created by Farzaad Goiporia on 2024-08-22.
+//
+
+import SwiftUI
+
+@main
+struct TodoList_RoRApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
